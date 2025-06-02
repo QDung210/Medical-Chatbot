f2eca171084157d7da834070bec559615e06bce5
QDung210 <dqdung205@gmail.com>
2025-06-02 09:41:45 +0700

    Update requirements.txt

--- rag_pipeline/__init__.py
+++ rag_pipeline/__init__.py
@@ -1,4 +1,4 @@
-from Rag_pipeline.src.rag_pipeline import create_pipeline
-from Rag_pipeline.src.config import DEFAULT_MODEL, DEFAULT_COLLECTION
+from rag_pipeline.src.rag_pipeline import create_pipeline
+from rag_pipeline.src.config import DEFAULT_MODEL, DEFAULT_COLLECTION
 
 __all__ = ['create_pipeline', 'DEFAULT_MODEL', 'DEFAULT_COLLECTION']
